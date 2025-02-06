de1c003ecf7e9010e2ddf35d604c68eb96b746a9
WinterSleep1 <131164420+WinterSleep1@users.noreply.github.com>
2025-02-06 19:40:50 +0900

    Add files via upload

---
+++ Program2/Class12th (Thread)/Class12th (Thread).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <thread>
+using namespace std;
+
+void Input()
+{
+	for (int i = 0; i < 10; i++) { cout << "input!" << endl; }
+}
+void Render()
+{
+	for (int i = 0; i < 10; i++) { cout << "render!" << endl; }
+}
+
+int main()
+{
+#pragma region 스레드
+	//	프로세스 내에서 실행되는 작업 단위
+// thread thread1(Input);
+// thread thread2(Render);
+// 
+// thread1.join();
+// thread2.join();
+#pragma endregion
+
+	return 0;
+}
